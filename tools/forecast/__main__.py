cea1a277e423c771a372100fa06c8fed1c7b661e
Anton Lashchanka <anton@rainbow.ai>
2026-03-26 12:12:09 +0100

    add configurable version for Rainbow API

--- tools/forecast/__main__.py
+++ tools/forecast/__main__.py
@@ -143,6 +143,7 @@ def _create_rainbow(args: argparse.Namespace) -> Rainbow:
                    chunk_size=args.chunk_size,
                    frequency=args.download_period,
                    token=args.token,
+                   version=args.version,
                    sensors=sensors)
 
 
@@ -281,6 +282,8 @@ if __name__ == "__main__":
     rainbow_parser = subparser.add_parser("rainbow", help="Rainbow")
     _add_sensors_params(rainbow_parser)
     rainbow_parser.add_argument("--token", type=str, required=True, help="Token to access Rainbow API")
+    rainbow_parser.add_argument("--version", type=str, required=False, default="v1", choices=Rainbow.API_VERSIONS,
+                                help=f"Rainbow API version. Values of {Rainbow.API_VERSIONS}")
     rainbow_parser.set_defaults(func=_create_rainbow)
 
     # WeatherCompany
--- tools/forecast/providers/rainbow.py
+++ tools/forecast/providers/rainbow.py
@@ -4,12 +4,19 @@ from typing_extensions import override  # for python <3.12
 
 
 class Rainbow(BaseForecastInPointProvider, RequestInterface):
+    """
+    Rainbow API versions:
+        - v1 provides radars coverage
+        - v2 provides global coverage
+    """
+    API_VERSIONS = ["v1", "v2"]
 
-    def __init__(self, token: str, *args, **kwargs):
+    def __init__(self, token: str, version: str = "v1", *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.token = token
+        self.version = version
 
     @override
     async def get_json_forecast_in_point(self, lon: float, lat: float) -> Response:
-        url = f"https://api.rainbow.ai/nowcast/v1/precip/{lon}/{lat}?token={self.token}"
+        url = f"https://api.rainbow.ai/nowcast/{self.version}/precip/{lon}/{lat}?token={self.token}"
         return await self._native_get(url=url)
