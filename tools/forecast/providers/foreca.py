fae4514f8dd708146bf3688558e38d376c5f8a4d
Anton Lashchanka <anton@rainbow.ai>
2026-06-28 17:44:01 +0200

    fix periods number

--- tools/forecast/providers/foreca.py
+++ tools/forecast/providers/foreca.py
@@ -27,7 +27,7 @@ class Foreca(BaseRateLimitedForecastInPointProvider, RequestInterface):
 
     @override
     async def get_json_forecast_in_point(self, lon: float, lat: float) -> Response:
-        resp = await self._native_get(url=f"{self._get_location_url(lon, lat)}?dataset=full",
+        resp = await self._native_get(url=f"{self._get_location_url(lon, lat)}?dataset=full&periods=12",
                                       headers={"Authorization": f"Bearer {self.token}"})
         if resp.ok:
             resp.payload = json.dumps({
