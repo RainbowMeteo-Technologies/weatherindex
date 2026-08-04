e773239380deb6f5d08cf0e090e2241eef7ebfe4
VIktar Astreika <viktor.astreyko@rainbow.ai>
2026-08-04 16:50:37 +0200

    Comment fixes

--- tests/metrics/calc/evaluator/test_ignore_precip_type.py
+++ tests/metrics/calc/evaluator/test_ignore_precip_type.py
@@ -44,7 +44,7 @@ class TestIgnorePrecipTypeEvaluator:
 
         result = worker._calculate(forecast_times=forecast_offsets,
                                    observations=observations,
-                                   forecast=forecast).metrics
+                                   forecast=forecast)
 
         merged_result = pandas.merge(expected_metrics,
                                      result,
@@ -85,7 +85,7 @@ class TestIgnorePrecipTypeEvaluator:
 
         result = worker._calculate(forecast_times=forecast_offsets,
                                    observations=observations,
-                                   forecast=forecast).metrics
+                                   forecast=forecast)
 
         result = result.groupby(["forecast_time"])[["tp", "tn", "fp", "fn"]].sum().reset_index()
         result["precision"] = result[["tp", "fp"]].apply(lambda row: precision(row["tp"], row["fp"]), axis=1)
